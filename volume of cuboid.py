2847590c69cbe0585de58e3616c290afc5ef47e3
iampriyamsaini <priyamsaini9@gmail.com>
2023-12-01 02:39:41 +0530

    Update volume of cuboid.py

--- volume of cuboid.py
+++ volume of cuboid.py
@@ -1,8 +1,6 @@
 length=eval(input("Enter length of the cuboid: "))
 breadth=eval(input("Enter breadth of the cuboid: "))
 height=eval(input("Enter height of the cuboid: "))
-import math
-pi=math.pi
 volume=length*breadth*height
 print(f"volume of cuboid is {volume}")
 
