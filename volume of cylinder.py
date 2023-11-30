4e0b21d98144cfbbd579e99620983e13cfaa80ae
iampriyamsaini <priyamsaini9@gmail.com>
2023-12-01 02:33:33 +0530

    Create volume of cylinder.py

---
+++ volume of cylinder.py
@@ -0,0 +1,6 @@
+radius=eval(input("Enter radius of the cylinder: "))
+height=eval(input("Enter height of the cylinder: "))
+import math
+pi=math.pi
+volume=pi*radius**2*height
+print(f"volume of cylinder is {volume}")
