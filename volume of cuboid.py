ada7d5dbb83e859d25a17a4e1d09dfe620fb1ebd
iampriyamsaini <priyamsaini9@gmail.com>
2023-12-01 02:37:44 +0530

    Create volume of cuboid.py

---
+++ volume of cuboid.py
@@ -0,0 +1,9 @@
+length=eval(input("Enter length of the cuboid: "))
+breadth=eval(input("Enter breadth of the cuboid: "))
+height=eval(input("Enter height of the cuboid: "))
+import math
+pi=math.pi
+volume=length*breadth*height
+print(f"volume of cuboid is {volume}")
+
+
