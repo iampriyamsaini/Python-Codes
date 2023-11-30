80792405b09ecfebce3859f80acfb497549a3e80
iampriyamsaini <priyamsaini9@gmail.com>
2023-11-30 18:42:16 +0530

    Update divisibility check.py

--- divisibility check.py
+++ divisibility check.py
@@ -1,5 +1,5 @@
 n=eval(input("Enter a number: "))
-d1=eval(input("Enter 1sr number: "))
+d1=eval(input("Enter 1st number: "))
 d2=eval(input("Enter 2nd number: "))
 if n%d1==0 and n%d2==0:
     print("Yes")
