47962b4f91930b69e367879769873ffb64e73803
iampriyamsaini <priyamsaini9@gmail.com>
2023-12-01 02:08:59 +0530

    Update weird_not weird .py

--- weird_not weird .py
+++ weird_not weird .py
@@ -1,3 +1,14 @@
+# Write a program for given an integer n, perform the following conditional actions:
+
+
+# If n is odd, print Weird
+# If n is even and in the inclusive range of 2 to 5, print Not Weird
+# If n is even and in the inclusive range of 6 to 20, print Weird
+# If n is even and greater than 20, print Not Weird
+
+
+
+
 n=float(input("Enter any number: "))
 if (n%2!=0):
  print("weird")
