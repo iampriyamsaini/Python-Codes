bf1c380899c4f9970db8ce7adca8065fee30d0e8
iampriyamsaini <priyamsaini9@gmail.com>
2023-11-30 21:30:18 +0530

    Update Take input data from user and give the data to user in sentence form by using f-string method .py

--- Take input data from user and give the data to user in sentence form by using f-string method .py
+++ Take input data from user and give the data to user in sentence form by using f-string method .py
@@ -2,9 +2,3 @@ a=input("Enter your Name:")
 b=input("Enter your University Name:")
 sentence= f"My name is {a}, \nI study at {b}"
 print(sentence)
-
-
-#by using f-string method
-
-'''here \n is used for break a line
- (or to make a sentence or paragraph in a new line)'''
