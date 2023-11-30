7c0cdd5a90a1b57bad5a9fde354ef809a6c3ca75
iampriyamsaini <priyamsaini9@gmail.com>
2023-12-01 02:29:42 +0530

    Create Volume of a cone.py

---
+++ Volume of a cone.py
@@ -0,0 +1,6 @@
+radius=eval(input("Enter radius of the cone: "))
+height=eval(input("Enter height of the cone: "))
+import math
+pi=math.pi
+volume=1/3*pi*radius**2*height
+print(f"volume of cone is {volume}")
