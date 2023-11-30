539de0fb80c562e43cb714d22e03533dcfba2790
iampriyamsaini <priyamsaini9@gmail.com>
2023-11-30 21:27:12 +0530

    Update and rename product of two numbers to product of three numbers.py

--- product of three numbers.py
+++ product of three numbers.py
@@ -1,4 +1,6 @@
+#product of three numbers
 num1=eval(input("Enter 1st number: "))
 num2=eval(input("Enter 2nd number: "))
-product=num1*num2
+num3=eval(input("Enter 3rd number: "))
+product=num1*num2*num3
 print(f"product of given numbers is {product}")
