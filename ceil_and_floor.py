55d1eec63b1fc9d2e5eecb70a149c82b66585a7b
Priyam Saini <priyamsaini9@gmail.com>
2023-12-28 21:23:30 +0530

    Create ceil_and_floor.py

---
+++ ceil_and_floor.py
@@ -0,0 +1,21 @@
+# python ceil function 
+a = 2
+b = 3
+import math as priyam
+print(priyam.ceil(7.8))
+# OUTPUT = 8
+
+
+
+
+# python floor function 
+a = 2
+b = 3
+import math as priyam
+print(priyam.floor(7.8))
+# OUTPUT = 7
+
+
+
+
+
